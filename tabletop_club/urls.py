7a619f7e0ab893b6517e64daac9edfead5414a38
JC8999 <S276053@uos.ac.uk>
2026-03-12 22:06:55 +0000

    Added templates.

--- tabletop_club/urls.py
+++ tabletop_club/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tabletop_club_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_admin, name='home_admin'),
+    path('member_home', views.home_member, name='home_member'),
+    path('game_catalogue', views.game_catalogue, name='game_catalogue'),
+    path('members', views.members, name='members'),
+    path('rentals', views.rentals, name='rentals'),
+    path('my_rentals', views.my_rentals, name='my_rentals'),
 ]
--- tabletop_club_app/views.py
+++ tabletop_club_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_admin(request):
+    return render(request, 'home_admin.html')
+
+def home_member(request):
+    return render(request, 'home_member.html')
+
+def game_catalogue(request):
+    return render(request, 'game_catalogue.html')
+
+def members(request):
+    return render(request, 'members.html')
+
+def rentals(request):
+    return render(request, 'rentals.html')
+
+def my_rentals(request):
+    return render(request, 'my_rentals.html')
