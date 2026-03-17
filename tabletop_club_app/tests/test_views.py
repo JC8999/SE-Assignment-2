dabc2145c5d146a516cbac268bd760d2d5313023
JC8999 <S276053@uos.ac.uk>
2026-03-17 15:40:54 +0000

    Added form tests.

--- tabletop_club_app/tests/test_views.py
+++ tabletop_club_app/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
+from tabletop_club_app.models import BoardGame
 
 class AuthenticationTests(TestCase):
     def setUp(self):
@@ -19,15 +20,41 @@ class AuthenticationTests(TestCase):
 
     def test_game_catalogue_requires_login(self):
         # Test to verify that a user that isn't logged in is redirected to login after trying to access the game catalogue page.
-        response = self.client.get(reverse("game_catalogue"))
+        response = self.client.get(reverse('game_catalogue'))
 
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, "/?next=/game_catalogue/")
+        self.assertRedirects(response, '/?next=/game_catalogue/')
 
     def test_members_page_admin_access(self):
         # Test to verify that the members page is only accessible to admins.
-        self.client.login(username="admin", password="testpassword")
+        self.client.login(username='admin', password='testpassword')
 
-        response = self.client.get(reverse("members"))
+        response = self.client.get(reverse('members'))
 
         self.assertEqual(response.status_code, 200)
+
+class CreateViewTests(TestCase):
+    def setUp(self):
+        # Test data to use in the create view tests.
+        self.admin = User.objects.create_user(
+            username='admin',
+            password='testpassword',
+            is_staff=True
+            )
+    def test_create_board_game_post(self):
+        # Test to verify that that submitting valid data creates an object and redirects.
+        self.client.login(username='admin', password='testpassword')
+
+        form_data = {
+            'title': 'Monopoly',
+            'category': 'strategy',
+            'min_players': 2,
+            'max_players': 6,
+            'playtime_minutes': 90,
+            'quantity': 5
+        }
+
+        response = self.client.post(reverse('create_board_game'), data=form_data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(BoardGame.objects.count(), 1)
---
+++ tabletop_club_app/tests/tests_forms.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from tabletop_club_app.forms import BoardGameCreationForm
+
+class BoardGameFormTest(TestCase):
+    # Test to verify that the board game creation forms accepts correct data.
+    def test_valid_board_game_form(self):
+        form_data = {
+            'title': 'Monopoly',
+            'category': 'strategy',
+            'min_players': 2,
+            'max_players': 6,
+            'playtime_minutes': 90,
+            'quantity': 5
+        }
+
+        form = BoardGameCreationForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_title_is_required(self):
+        # Test to verify that required fields are enforced during creation form.
+        form_data = {
+            'title': '',
+            'category': 'Strategy',
+            'min_players': 3,
+            'max_players': 4,
+            'playtime_minutes': 90,
+            'quantity': 5
+        }
+
+        form = BoardGameCreationForm(data=form_data)
+
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors)
+
+    def test_min_players_cannot_exceed_max_players(self):
+        # Test to verify that custom validation logic is enforced during creation form.
+        form_data = {
+            'title': 'Chess',
+            'category': 'Strategy',
+            'min_players': 5,
+            'max_players': 2,
+            'playtime_minutes': 30,
+            'quantity': 2
+        }
+
+        form = BoardGameCreationForm(data=form_data)
+
+        self.assertFalse(form.is_valid())
